892a1c7d9f460d52e004e5776253e80d211e9a62
bgettys <bob@bobgettys.com>
2014-02-25 19:36:22 -0500

    Added some comments

--- p18-67/src/main/java/main/Main.java
+++ p18-67/src/main/java/main/Main.java
@@ -7,8 +7,17 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * @author bgettys
+ *
+ */
 public class Main {
 	
+	/**
+	 *  The number triangle is in the form of a tree except leaves can share parents.
+	 *  This helps to keep track of the structure as well as the totals as they're calculated.	 * 
+	 */
 	private static class Node {
 		
 		public final int value;
@@ -29,29 +38,37 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 		try {
 			String line;
+			// This null is just here for the first number to have an extra parent without special code inside the loop
 			nodeQueue.addLast(null);
 			while (null != (line = br.readLine())) {
+				// Add a null to make creating new child nodes from the left side a bit easier (on each level the first left parent will be null)
 				nodeQueue.addLast(null);
 				StringTokenizer st = new StringTokenizer(line, " ");
 				while (st.hasMoreTokens()) {
+					/* Each child node will point to two parents (or a null) and remove one parent
+					 * from consideration as the tree is traversed
+					 */
 					nodeQueue.addLast(new Node(Integer.parseInt(st.nextToken()), nodeQueue.pollFirst(), nodeQueue.peekFirst()));
 				}
 			}
 		} finally {
 			br.close();
 		}
+		// Remove that special left-side null from before
 		nodeQueue.removeFirst();
+		/* Start moving back up the tree while updating totals and
+		 * discarding the leaves as we're done with them
+		 */
 		Node node;
 		while (null != (node = nodeQueue.pollFirst())) {
 			updateTotal(node.total, node.leftParent);
 			updateTotal(node.total, node.rightParent);
 			if (node.leftParent != null) {
 				nodeQueue.addLast(node.leftParent);
-			} else if (node.rightParent == null) {
+			} else if (node.rightParent == null) { // if both parents are null then we found the root
 				System.out.println(node.total);
 			}
 		}
-		
 	}
 	
 	private static void updateTotal(int childTotal, Node parent) {
